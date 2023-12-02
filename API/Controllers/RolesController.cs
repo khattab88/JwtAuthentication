a2c9a4125ccd5fe1531b268eaaa8c16381ee8468
khattab88 <m_khatab_88@hotmail.com>
2023-12-02 23:03:03 +0200

    create roles controller

---
+++ API/Controllers/RolesController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RolesController> _logger;
+
+        public RolesController(
+            AppDbContext db,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ILogger<RolesController> logger
+            )
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRoles()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return Ok(roles);
+        }
+    }
+}
