dda3698c2b87cae3ee01a249948a485ebad544a9
khattab88 <m_khatab_88@hotmail.com>
2023-12-03 14:06:56 +0200

    add claim to user

--- API/Controllers/ClaimsController.cs
+++ API/Controllers/ClaimsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -35,11 +37,33 @@ namespace API.Controllers
             var user = await _userManager.FindByEmailAsync(userEmail);
             if(user == null)
             {
-                return NotFound();
+                return NotFound(new { Error = "User not found" });
             }
 
             var userClaims = await _userManager.GetClaimsAsync(user);
             return Ok(userClaims);
+        }
+
+        [HttpPost]
+        [Route("AddClaimToUser")]
+        public async Task<IActionResult> AddClaimToUser(string email, string claimName, string claimValue)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return NotFound(new { Error = "User not found" });
+            }
+
+            var userClaim = new Claim(claimName, claimValue);
+
+            var result = await _userManager.AddClaimAsync(user, userClaim);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new { Error = "Unable to add claim" });
+            }
+
+            return Ok(new { Message = "Claim added to user" });
         }
     }
 }
