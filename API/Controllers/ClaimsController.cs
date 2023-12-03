bcc486f9c449dce3bc3ad184aed5e8e6e73346fb
khattab88 <m_khatab_88@hotmail.com>
2023-12-03 13:48:13 +0200

    get all claims

---
+++ API/Controllers/ClaimsController.cs
@@ -0,0 +1,45 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClaimsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<ClaimsController> _logger;
+
+        public ClaimsController(
+            AppDbContext db,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ILogger<ClaimsController> logger
+            )
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllClaims(string userEmail)
+        {
+            var user = await _userManager.FindByEmailAsync(userEmail);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            return Ok(userClaims);
+        }
+    }
+}
